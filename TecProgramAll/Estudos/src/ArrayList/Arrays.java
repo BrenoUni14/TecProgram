80b9c537adbd7d709aed210101de30e598809b99
BrenoUni14 <breno.carmo01@fatec.sp.gov.br>
2023-05-02 10:50:14 -0300

    Start of ArrayList studies

--- TecProgramAll/Estudos/src/ArrayList/Arrays.java
+++ TecProgramAll/Estudos/src/ArrayList/Arrays.java
@@ -1,2 +1,9 @@
-package ArrayList;public class Arrays {
+package ArrayList;
+import java.util.ArrayList;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<String>();
+    }
 }
