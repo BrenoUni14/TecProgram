4e71df27135e7f3160d21f80e9b49de74da6212f
BrenoUni14 <breno.carmo01@fatec.sp.gov.br>
2023-05-02 10:49:48 -0300

    End of arrays studies

--- TecProgramAll/Estudos/src/Vetores/Exemplo.java
+++ TecProgramAll/Estudos/src/Vetores/Exemplo.java
@@ -9,6 +9,10 @@ public class Exemplo {
             vetor.adiciona("elemento 1");
             vetor.adiciona("elemento 2");
             vetor.adiciona("elemento 3");
+            vetor.adiciona("elemento 4");
+            vetor.adiciona("elemento 5");
+            vetor.adiciona("elemento 6");
+            vetor.adiciona("elemento 7");
         }catch (Exception e){
             e.printStackTrace();
         }
@@ -16,11 +20,19 @@ public class Exemplo {
         System.out.println(vetor.tamanho());
         System.out.println(vetor.toString());
 
+        System.out.println("");
         System.out.println(vetor.adicionaLugar(0, "elemento 0"));
         System.out.println(vetor.tamanho());
         System.out.println(vetor.toString());
 
+        System.out.println("");
         System.out.println(vetor.busca(1));
+        System.out.println(vetor.busca1("elemento 3"));
+
+        System.out.println("");
+        vetor.removeLugar(4);
+        System.out.println(vetor.tamanho());
+        System.out.println(vetor.toString());
 
 
     }
--- TecProgramAll/Estudos/src/Vetores/Vetor2.java
+++ TecProgramAll/Estudos/src/Vetores/Vetor2.java
@@ -1,7 +1,5 @@
 package Vetores;
 
-import java.util.Arrays;
-
 public class Vetor2 {
     private String[] elementos;
     private int tamanho;
@@ -12,6 +10,7 @@ public class Vetor2 {
     }
 
     public void adiciona (String elemento) throws Exception{
+        this.aumentaCapacidade();
             if (this.elementos[this.tamanho] == null){
                 this.elementos[this.tamanho] = elemento;
                 this.tamanho++;
@@ -50,16 +49,47 @@ public class Vetor2 {
         }
     }
 
+    public int busca1(String elemento) throws Exception{
+        for (int i = 0; i < tamanho; i++){
+            if (elementos[i].equalsIgnoreCase(elemento)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public boolean adicionaLugar(int posicao, String elemento) throws Exception{
+        this.aumentaCapacidade();
         if(posicao >= 0 && posicao < tamanho){
-            for (int i = this.tamanho - 1; i > posicao; i--){
+            for (int i = this.tamanho - 1; i > posicao - 1; i--){
                 this.elementos[i + 1] = this.elementos[i];
             }
             this.elementos[posicao] = elemento;
             this.tamanho++;
-        }else{
+        } else{
             throw new Exception("Posição Inválida");
         }
         return true;
+    }
+
+    private void aumentaCapacidade(){
+        if (this.tamanho == this.elementos.length){
+            String [] elementosNovos = new String[this.elementos.length * 2];
+            for (int i = 0; i < this.elementos.length; i++){
+                elementosNovos[i] = this.elementos[i];
+            }
+            this.elementos = elementosNovos;
+        }
+    }
+
+    public void removeLugar(int posicao) throws Exception{
+        if(posicao >= 0 && posicao < tamanho){
+            for (int i = posicao; i < this.tamanho-1; i++){
+                this.elementos[i] = this.elementos[i + 1];
+            }
+            this.tamanho--;
+        } else{
+            throw new Exception("Posição Inválida");
+        }
     }
 }
